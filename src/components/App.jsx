ae582da32a0097ac49944b8dc01d2d7ef93c29b0
Zoja7 <yaozonka@gmail.com>
2023-10-12 21:57:38 +0300

    commit_3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import  user  from '../data/user.json';
+import { Profile } from './Profile/Profile';
+
+import statistics from '../data/statistics.json';
+import { Statistics } from './Statistics/Statistics';
+import { Fragment } from 'react';
+
+
 export const App = () => {
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+    <Fragment>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+     
+      <Statistics
+        title="Upload stats"
+        stats={statistics}
+      />
+    </Fragment>   
+  ) 
+  
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,18 @@
+export const Statistics = ({ title=false, stats }) => { 
+
+    return (
+        <section className="statistics">
+            {title && <h2 className="title">"Upload stats"</h2>}
+            
+            <ul className="stat-list">
+                {stats.map(stats => {
+                    return (<li className="item">
+                        <span className="label">{stats.label}</span>
+                        <span className="percentage">{stats.percentage}</span>
+                    </li>)
+                })}
+                
+            </ul>
+        </section> 
+    )
+}
